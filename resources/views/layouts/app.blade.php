27ad76da64b3cd7990aa34c5c34538e6e8bf2dbd
clmb00 <paolo.colombo2000@gmail.com>
2022-12-22 17:26:37 +0100

    [fix] favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ Vite::asset('/resources/img/favicon.ico') }}" type="image/x-icon">
     @vite('resources/js/app.js')
-    <title>DC | @yield('title')</title>
+    <title>Laravel DC | @yield('title')</title>
 </head>
 <body>
 
