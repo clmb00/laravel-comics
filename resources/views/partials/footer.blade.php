ceed12ac1ea5b550df25926aca6f2508b34cbef2
clmb00 <paolo.colombo2000@gmail.com>
2022-12-22 16:35:26 +0100

    [add] footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,60 @@
-
+<footer>
+    <div class="container">
+        <div id="left">
+          <nav>
+            <div>
+              <h3>Dc Comics</h3>
+              <ul>
+                <li><a href="#">Charachters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+            </div>
+            <div>
+              <h3>Shop</h3>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
+            <div>
+              <h3>Sites</h3>
+              <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+    </div>
+    <div id="social-bar">
+        <div class="container">
+          <button>Sign-up Now!</button>
+          <div class="social">
+            <a href="#">Follow Us</a>
+            <div class="btn">
+              <a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a>
+            </div>
+            <div class="btn">
+              <a href="#"><img src="../assets/img/footer-twitter.png" alt=""></a>
+            </div>
+            <div class="btn">
+              <a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a>
+            </div>
+            <div class="btn">
+              <a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+            </div>
+            <div class="btn">
+              <a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a>
+            </div>
+          </div>
+        </div>
+    </div>
+</footer>
