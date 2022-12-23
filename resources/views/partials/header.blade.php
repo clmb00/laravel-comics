c5c8619e1e24e8a927a8cb9bf4116b169b8cb1a8
clmb00 <paolo.colombo2000@gmail.com>
2022-12-23 15:16:22 +0100

    [fix] header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,28 @@
 <header>
-    <div class="container">
-      <div class="logo">
-        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
-      </div>
-      <nav>
-        <ul>
-            @foreach (config('db.main_menu') as $menu)
-                <li class="{{Route::currentRouteName() == $menu["href"] ? 'active' : ''}}"><a href="{{ route($menu["href"]) }}">{{$menu["pageName"]}}</a></li>
-            @endforeach
-        </ul>
-      </nav>
+    <div class="top-header">
+        <div class="container">
+            <a href="#">DC POWER&#x2122; VISA&#xae;</a>
+            <a href="#">ADDITIONAL DC SITES</a>
+        </div>
+    </div>
+    <div class="bottom-header">
+        <div class="container">
+          <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+          </div>
+          <nav>
+            <ul>
+                @foreach (config('db.main_menu') as $menu)
+                    <li class="{{Route::currentRouteName() == $menu["href"] ? 'active' : ''}}"><a href="{{ route($menu["href"]) }}">{{ strtoupper($menu["pageName"])}}</a></li>
+                @endforeach
+            </ul>
+          </nav>
+          <div class="search-input">
+              <input type="text" name="search" id="search" placeholder="Search">
+              <div class="search-icon">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+              </div>
+          </div>
+        </div>
     </div>
 </header>
